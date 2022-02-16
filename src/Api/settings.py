91c03e0c4b1e64ee84b1501b5dc187b56a187113
Zeldodo17 <meza.emilio.2019@cesunbc.edu.mx>
2022-02-16 01:44:44 -0800

    Se agrego mas configuracion a los CORS

--- src/Api/settings.py
+++ src/Api/settings.py
@@ -145,17 +145,19 @@ USE_I18N = True
 
 USE_TZ = True
 
+CORS_ALLOW_ALL_ORIGINS = True # If this is used then CORS_ALLOWED_ORIGINS will not have any effect
+CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000'
-    'http://localhost:4000'
-    'http://localhost:5000'
+    'http://localhost:3000',
+] # If this is used, then not need to use CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    'http://localhost:3000',
 ]
-
-CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000'
-    'http://localhost:4000'
-    'http://localhost:5000'
-]
+ROOT_URLCONF = 'Api.urls'
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost',
+    'http://localhost:3000',
+    'http://localhost:4200',)
 
 
 # Static files (CSS, JavaScript, Images)
